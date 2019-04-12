8f12e56050786f46557434b900c8e36f5df1dd1d
Eric Nunn <lowlyherotech@gmail.com>
2019-04-12 11:42:02 -0400

    Request 1 completed in Arrays challenge

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -54,7 +54,7 @@ console.log(tRex.period);
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
 
 tRex.roar = function(){
-  console.log('RAWERSRARARWERSARARARRRR!');
+  return 'RAWERSRARARWERSARARARRRR!';
 }
 
 console.log(tRex.roar());
@@ -79,7 +79,11 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
-console.log(universities)
+
+for (let i = 0; i < graduates.length; i++){
+    universities.push(graduates[i].university);
+}
+console.log(universities.sort());
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
