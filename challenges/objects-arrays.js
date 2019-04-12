1fd70288ec90b509bd92cf813a9ee5a088f0c639
BrennanHolm <brennan.mholmich@gmail.com>
2019-04-12 11:27:41 -0500

    Add console log answers
    Adds the console log answers for the cretassic objects in
    objects-arrays.js

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -17,7 +17,10 @@ let cretassic = {
     diet: 'carnivorous',
     weight: 7000,
     length: 12,
-    period: 'Late Cretaceious'
+    period: 'Late Cretaceious',
+    roar: function() {
+      return `RAWERSRARARWERSARARARRRR!`
+    }
   },
   stegosaurus: {
     name: 'stegosaurus',
@@ -38,20 +41,20 @@ let cretassic = {
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(cretassic.tyrannosaurus.weight + "kg");
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(cretassic.velociraptor.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(cretassic.stegosaurus.length + "m");
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(cretassic.tyrannosaurus.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(cretassic.tyrannosaurus.roar());
 
 
 // ==== Arrays ====
